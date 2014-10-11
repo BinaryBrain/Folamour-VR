0d6b8f1a54d3ea85ee70f415e13c5e87c9384694
Corentin <corentin-p@sunrise.ch>
2014-10-11 12:25:31 +0200

    refactoring

---
+++ tests/camera.js
@@ -0,0 +1,56 @@
+//Camera initialization
+function initCamera(scene)
+{
+    var camera=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    var cameraHitbox = new Physijs.BoxMesh(
+        new THREE.CylinderGeometry(4, 4, 12),
+        0,
+        50 // mass
+    );
+    cameraHitbox.position.set(0,57,0);
+    cameraHitbox.add(camera);
+    scene.add(cameraHitbox);
+    //Constraints
+    var constraint = new Physijs.DOFConstraint(
+        cameraHitbox, // First object to be constrained
+        0,
+        new THREE.Vector3( 0, 10, 0 ) // point in the scene to apply the constraint
+    );
+    scene.addConstraint(constraint);
+    constraint.setLinearLowerLimit( new THREE.Vector3( -1e30, -1e30, -1e30) ); // sets the lower end of the linear movement along the x, y, and z axes.
+    constraint.setLinearUpperLimit( new THREE.Vector3( 1e30, 1e30, 1e30 ) );
+    constraint.setAngularLowerLimit( new THREE.Vector3( 0, 0, 0 ) ); // sets the lower end of the angular movement, in radians, along the x, y, and z axes.
+    constraint.setAngularUpperLimit( new THREE.Vector3( 0, 0, 0 ) ); // sets the upper end of the angular movement, in radians, along the x, y, and z axes.
+
+    return {camera:camera, cameraHitbox:cameraHitbox};
+}
+
+//Set camera speed
+function setSpeed() {
+    var speed = [0,0,0];
+    if (pressed.w) {
+        speed[0] += Math.sin(theta);
+        speed[2] += Math.cos(theta);
+    }
+
+    if (pressed.s) {
+        speed[0]+=-Math.sin(theta);
+        speed[2]+=-Math.cos(theta);
+    }
+
+    if (pressed.a) {
+        speed[0]+=Math.sin(theta+Math.PI/2);
+        speed[2]+=Math.cos(theta+Math.PI/2);
+    }
+
+    if (pressed.d) {
+        speed[0]+= Math.sin(theta-Math.PI/2);
+        speed[2]+= Math.cos(theta-Math.PI/2);
+    }
+
+    if (pressed.jump) {
+        if (Math.abs(cameraHitbox.getLinearVelocity().y) < 0.1) {
+            speed[1]+=20;
+        }
+    }
+}
---
+++ tests/createBox.js
@@ -0,0 +1,29 @@
+//Spawn a box randomly
+function createBox(scene)
+{
+    //Material
+    var material = Physijs.createMaterial(
+	new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/plywood.jpg' ) }),
+	    .6, // medium friction
+	    .3 // low restitution
+    );
+    material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+    material.map.repeat.set(0.5,0.5);
+    //Mesh
+    var box_geometry = new THREE.CubeGeometry( 4, 4, 4 );
+    var box = new Physijs.BoxMesh(
+        box_geometry,
+        material
+    );
+    //Collisions handling
+    var handleCollision = function(collided_with, linearVelocity, angularVelocity) {}
+    box.collisions = 0;
+    box.position.set(Math.random() * 15 - 7.5,25,Math.random() * 15 - 7.5);           
+    box.rotation.set(Math.random() * Math.PI,Math.random() * Math.PI,Math.random() * Math.PI);           
+    box.castShadow = true;
+    //Listeners
+    box.addEventListener('collision', handleCollision);
+    //box.addEventListener( 'ready', spawnBox );
+    scene.add(box);
+    setTimeout(function () { createBox(scene); }, 1000);
+}
